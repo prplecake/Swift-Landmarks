ea49b18f9d3847c4d80e2e29d73a66ece3fa42c2
Matthew Jorgensen <me@prplecake.com>
2023-05-20 23:09:09 -0500

    Initial commit
    State after "Drawing Paths and Shapes" tutorial
    <https://developer.apple.com/tutorials/swiftui/drawing-paths-and-shapes>

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,35 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by prplecake on 5/20/23.
+//
+
+import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+    
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't file \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from mail bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
